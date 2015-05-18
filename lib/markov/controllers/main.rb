c724067769769a32d8a1cd28f58735149d67c061
Michael Kuehl <mickuehl@ratchet.cc>
2015-05-18 19:30:39 +0200

    some tweeks to sentence generation and new/updated seed text

--- lib/markov/controllers/main.rb
+++ lib/markov/controllers/main.rb
@@ -18,7 +18,7 @@ module Markov
     end
           
     get "/" do
-      @sentences = @@markov.generate_sentence 20
+      @sentences = @@markov.generate_sentence 24
       erb :markov
     end
     

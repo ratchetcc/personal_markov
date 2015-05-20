a1c0355581623604c84fd2957acf26203c197584
Michael Kuehl <mickuehl@ratchet.cc>
2015-05-20 11:49:11 +0200

    increased the sentence length

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,5 @@
 class MainController < ApplicationController
   def index
-    @markov = Rails.application.config.markov_generator.generate_sentence
+    @markov = Rails.application.config.markov_generator.generate_sentence 60
   end
 end

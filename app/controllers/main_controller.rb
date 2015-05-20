5e93aa3d81825729b65c025f4b1b26c727c46efe
Michael Kuehl <mickuehl@ratchet.cc>
2015-05-20 14:04:35 +0200

    updated fontawesome, new version is now 4.3.0

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,5 @@
 class MainController < ApplicationController
   def index
-    @markov = Rails.application.config.markov_generator.generate_sentence 60
+    @markov = Rails.application.config.markov_generator.generate_sentence 40
   end
 end
